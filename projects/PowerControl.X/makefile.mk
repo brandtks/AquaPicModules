7bb3998f40a7bc77cae4de77bf680b3844290e99
Skyler Brandt <brandtsa@live.com>
2017-06-28 18:00:22 -0400

    work on makefiles

--- projects/PowerControl.X/makefile.mk
+++ projects/PowerControl.X/makefile.mk
@@ -1,34 +1,40 @@
 CC=xc8
-OBJ_DIR=xbuild
-DIST_DIR=xdist
+MODE=free
+WARN=0
+#ASMLIST=
+ASMLIST=--asmlist
+
+OBJ_DIR=build/PowerControl
+DIST_DIR=dist/PowerControl
 MKDIR=mkdir -p
-RM=rm -f
+RM=rm -rf
+MV=mv
 
 CHIP=16F1937
-OBJS=xbuild/PowerControl.p1 xbuild/adc.p1 xbuild/uart.p1 xbuild/aquaPicBus.p1
+OBJS=$(OBJ_DIR)/PowerControl/PowerControl.p1 $(OBJ_DIR)/adc/adc.p1 $(OBJ_DIR)/uart/uart.p1 $(OBJ_DIR)/aquaPicBus/aquaPicBus.p1
 PROJ_DEPS=../../lib/adc/adc.h ../../lib/aquaPicBus/aquaPicBus.h ../../lib/common/common.h ../../lib/led/led.h ../../lib/pins/pins.h bsp.h
 
 $(DIST_DIR)/PowerControl.hex : $(OBJS)
+	@$(RM) $(DIST_DIR)
 	@$(MKDIR) $(DIST_DIR)
-	@$(RM) $(DIST_DIR)/PowerControl.hex
-	$(CC) --chip=$(CHIP) -O$(DIST_DIR)/PowerControl.hex $(OBJS)
+	$(CC) --chip=$(CHIP) -Q -G${DIST_DIR}/PowerControl.sym -M${DIST_DIR}/PowerControl.map --double=24 --float=24 --opt=+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist -DXPRJ_default=default --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s" --memorysummary $(DIST_DIR)/memoryfile.xml -O$(DIST_DIR)/PowerControl.elf $(OBJS)
 
-$(OBJ_DIR)/PowerControl.p1 : PowerControl.c $(PROJ_DEPS)
-	@$(MKDIR) $(OBJ_DIR)
-	@-$(RM) $(OBJ_DIR)/PowerControl.p1
-	$(CC) --chip=$(CHIP) --pass1 -O$(OBJ_DIR)/PowerControl.p1 PowerControl.c
+$(OBJ_DIR)/PowerControl/PowerControl.p1 : PowerControl.c $(PROJ_DEPS)
+	@$(RM) $(OBJ_DIR)/PowerControl
+	@$(MKDIR) $(OBJ_DIR)/PowerControl
+	$(CC) --pass1 --chip=$(CHIP) -Q --double=24 --float=24 --opt=+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=$(MODE) -P --warn=$(WARN) $(ASMLIST) --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s" -O$(OBJ_DIR)/PowerControl/PowerControl.p1 PowerControl.c
 
-$(OBJ_DIR)/adc.p1 : ../../lib/adc/adc.c ../../lib/adc/adc.h
-	@$(MKDIR) $(OBJ_DIR)
-	@-$(RM) $(OBJ_DIR)/adc.p1
-	$(CC) --chip=$(CHIP) --pass1 -O$(OBJ_DIR)/adc.p1 ../../lib/adc/adc.c
+$(OBJ_DIR)/adc/adc.p1 : ../../lib/adc/adc.c ../../lib/adc/adc.h
+	@$(RM) $(OBJ_DIR)/adc
+	@$(MKDIR) $(OBJ_DIR)/adc
+	$(CC) --pass1 --chip=$(CHIP) -Q --double=24 --float=24 --opt=+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=$(MODE) -P --warn=$(WARN) $(ASMLIST) --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s" -O$(OBJ_DIR)/adc/adc.p1 ../../lib/adc/adc.c
 
-$(OBJ_DIR)/uart.p1 : ../../lib/uart/uart.c ../../lib/uart/uart.h
-	@$(MKDIR) $(OBJ_DIR)
-	@$(RM) $(OBJ_DIR)/uart.p1
-	$(CC) --chip=$(CHIP) --pass1 -O$(OBJ_DIR)/uart.p1 ../../lib/uart/uart.c
-
-$(OBJ_DIR)/aquaPicBus.p1 : ../../lib/aquaPicBus/aquaPicBus.c ../../lib/aquaPicBus/aquaPicBus.h
-	@$(MKDIR) $(OBJ_DIR)
-	@$(RM) $(OBJ_DIR)/aquaPicBus.p1
-	$(CC) --chip=$(CHIP) --pass1 -O$(OBJ_DIR)/aquaPicBus.p1 ../../lib/aquaPicBus/aquaPicBus.c
+$(OBJ_DIR)/uart/uart.p1 : ../../lib/uart/uart.c ../../lib/uart/uart.h
+	@$(RM) $(OBJ_DIR)/uart
+	@$(MKDIR) $(OBJ_DIR)/uart
+	$(CC) --pass1 --chip=$(CHIP) -Q --double=24 --float=24 --opt=+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=$(MODE) -P --warn=$(WARN) $(ASMLIST) --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s" -O$(OBJ_DIR)/uart/uart.p1 ../../lib/uart/uart.c
+	
+$(OBJ_DIR)/aquaPicBus/aquaPicBus.p1 : ../../lib/aquaPicBus/aquaPicBus.c ../../lib/aquaPicBus/aquaPicBus.h 
+	@$(RM) $(OBJ_DIR)/aquaPicBus
+	@$(MKDIR) $(OBJ_DIR)/aquaPicBus
+	$(CC) --pass1 --chip=$(CHIP) -Q --double=24 --float=24 --opt=+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=$(MODE) -P --warn=$(WARN) $(ASMLIST) --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s" -O$(OBJ_DIR)/aquaPicBus/aquaPicBus.p1 ../../lib/aquaPicBus/aquaPicBus.c
