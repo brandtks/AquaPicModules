f5dffb476165f88afe61bfe30c7566ad22b3d247
Skyler Brandt <brandtsa@live.com>
2017-11-14 09:10:01 -0500

    started work on 32bit ai firmware

--- Firmware/projects/AnalogInputCard.X/main.c
+++ Firmware/projects/AnalogInputCard.X/main.c
@@ -1,71 +1,46 @@
-/**
-  Generated Main Source File
-
-  Company:
-    Microchip Technology Inc.
-
-  File Name:
-    main.c
-
-  Summary:
-    This is the main file generated using PIC24 / dsPIC33 / PIC32MM MCUs
-
-  Description:
-    This header file provides implementations for driver APIs for all modules selected in the GUI.
-    Generation Information :
-        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - pic24-dspic-pic32mm : v1.35
-        Device            :  PIC32MM0064GPM028
-    The generated drivers are tested against the following:
-        Compiler          :  XC32 1.42
-        MPLAB             :  MPLAB X 3.60
-*/
+/*
+ * Analog Input Card - main.c
+ *
+ * Created 2017 Skyler Brandt
+ *
+ * Description:
+ *  Firmware for the analog input card
+ * 
+ * Device:
+ *  PIC32MM0064GPM028
+ */
 
 /*
-    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
-    software and any derivatives exclusively with Microchip products.
-
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
-    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-
-    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
-    TERMS.
-*/
+ * Copyright (c) 2017 Goodtime Development
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+ */
 
 #include "mcc_generated_files/mcc.h"
 
-/*
-                         Main application
- */
-void main(void)
-{
-    // initialize the device
+void main(void) {
+    /* initialize the device */
     SYSTEM_Initialize();
 
-    // When using interrupts, you need to set the Global Interrupt Enable bits
-    // Use the following macros to:
 
-    // Enable the Global Interrupts
+    /* Enable the Global Interrupts */
     INTERRUPT_GlobalEnable();
 
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalDisable();
-
-    while (1)
-    {
-        // Add your application code
+    while (1) {
+        
     }
 }
-/**
- End of File
-*/
